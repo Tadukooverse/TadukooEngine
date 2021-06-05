808d026bfc9fbc96d77c626b294e2e34c00cf661
Tadukoo <realtadukoo@gmail.com>
2021-06-05 16:47:43 -0400

    Added ShortInfo
    - Part of Issue #4

---
+++ TadukooEngine/junit/com/github/tadukoo/engine/info/ShortInfoTest.java
@@ -0,0 +1,61 @@
+package com.github.tadukoo.engine.info;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ShortInfoTest{
+	private ShortInfo info;
+	private final InfoType type = InfoType.PROGRAM;
+	private final String title = "Test Program";
+	private final String name = "TestName.json";
+	private final String location = "https://dummy.url/TestName.json";
+	
+	@BeforeEach
+	public void setup(){
+		info = new ShortInfo(type, title, name, location);
+	}
+	
+	@Test
+	public void testGetType(){
+		assertEquals(type, info.getType());
+	}
+	
+	@Test
+	public void testGetTitle(){
+		assertEquals(title, info.getTitle());
+	}
+	
+	@Test
+	public void testGetInfoName(){
+		assertEquals(name, info.getInfoName());
+	}
+	
+	@Test
+	public void testGetInfoLocation(){
+		assertEquals(location, info.getInfoLocation());
+	}
+	
+	@Test
+	public void testMappedPojoConstructor(){
+		ShortInfo otherInfo = new ShortInfo(type, title, name, location);
+		info = new ShortInfo(otherInfo);
+		assertEquals(type, info.getType());
+		assertEquals(title, info.getTitle());
+		assertEquals(name, info.getInfoName());
+		assertEquals(location, info.getInfoLocation());
+	}
+	
+	@Test
+	public void testGetKeyOrder(){
+		List<String> keys = info.getKeyOrder();
+		assertEquals(4, keys.size());
+		assertEquals(InfoType.KEY, keys.get(0));
+		assertEquals("title", keys.get(1));
+		assertEquals("info-name", keys.get(2));
+		assertEquals("info-location", keys.get(3));
+	}
+}
---
+++ TadukooEngine/src/com/github/tadukoo/engine/info/ShortInfo.java
@@ -0,0 +1,81 @@
+package com.github.tadukoo.engine.info;
+
+import com.github.tadukoo.parsing.json.AbstractOrderedJSONClass;
+import com.github.tadukoo.util.ListUtil;
+import com.github.tadukoo.util.pojo.MappedPojo;
+
+import java.util.List;
+
+/**
+ * Short Info is used in lists of info to provide basic information for finding program or library info files.
+ *
+ * @author Logan Ferree (Tadukoo)
+ * @version Alpha v.0.1
+ */
+public class ShortInfo extends AbstractOrderedJSONClass{
+	/** The key for the title of the info */
+	private static final String TITLE = "title";
+	/** The key for the name of the info file */
+	private static final String INFO_NAME = "info-name";
+	/** The key for the location (online) of the info file */
+	private static final String INFO_LOCATION = "info-location";
+	
+	/**
+	 * Creates a new Short Info object with the given parameters.
+	 *
+	 * @param type The type of Short Info
+	 * @param title The title of the info
+	 * @param infoName The name of the info file
+	 * @param infoLocation The location (online) of the info file, if it needs downloaded
+	 */
+	public ShortInfo(InfoType type, String title, String infoName, String infoLocation){
+		super();
+		setItem(InfoType.KEY, type.getType());
+		setItem(TITLE, title);
+		setItem(INFO_NAME, infoName);
+		setItem(INFO_LOCATION, infoLocation);
+	}
+	
+	/**
+	 * Creates a new Short Info object with the values of the given pojo.
+	 *
+	 * @param pojo The {@link MappedPojo} to use the values of
+	 */
+	public ShortInfo(MappedPojo pojo){
+		super(pojo);
+	}
+	
+	/** {@inheritDoc} */
+	@Override
+	public List<String> getKeyOrder(){
+		return ListUtil.createList(InfoType.KEY, TITLE, INFO_NAME, INFO_LOCATION);
+	}
+	
+	/**
+	 * @return The type of Short Info
+	 */
+	public InfoType getType(){
+		return InfoType.parseFromString((String) getItem(InfoType.KEY));
+	}
+	
+	/**
+	 * @return The title of the info
+	 */
+	public String getTitle(){
+		return (String) getItem(TITLE);
+	}
+	
+	/**
+	 * @return The name of the info file
+	 */
+	public String getInfoName(){
+		return (String) getItem(INFO_NAME);
+	}
+	
+	/**
+	 * @return The location (online) of the info file, if it needs downloaded
+	 */
+	public String getInfoLocation(){
+		return (String) getItem(INFO_LOCATION);
+	}
+}
